7a8a1967765aee51f7030f0c47b3df0cf501b068
Petr Houška <houskape@gmail.com>
2017-10-12 18:20:18 +0200

    Initial commit of splay tree.

---
+++ SplayTree/SplayTree.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SplayTree
+{
+    class SplayNode
+    {
+        public int Key { get; private set; }
+
+        public SplayNode LeftSon { get; set; }
+        public SplayNode RightSon { get; set; }
+
+        public void Insert(int key)
+        {
+
+        }
+
+        public void Find(int key)
+        {
+
+        }
+
+    }
+}
