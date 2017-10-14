21a5788595936c8e47e3b7280300aa4725974dad
Petr Houška <houskape@gmail.com>
2017-10-14 22:03:35 +0200

    Simple splaying with single rotations.

--- SplayTree/SplayTree.cs
+++ SplayTree/SplayTree.cs
@@ -1,7 +1,10 @@
-﻿using System;
+﻿#define SIMPLESPLAY
+
+using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Text;
+
 
 namespace SplayTree
 {
@@ -24,12 +27,12 @@ namespace SplayTree
                 if (newKey == currKey) { return; }
                 else if (newKey > currKey)
                 {
-                    if (currNode.RightSon == null) { currNode.RightSon = newNode; newNode.Parent = currNode; return; }
+                    if (currNode.RightSon == null) { currNode.RightSon = newNode; newNode.Parent = currNode; Splay(currNode); return; }
                     else { currNode = currNode.RightSon; continue; }
                 }
                 else if (newKey < currKey)
                 {
-                    if (currNode.LeftSon == null) { currNode.LeftSon = newNode; newNode.Parent = currNode; return; }
+                    if (currNode.LeftSon == null) { currNode.LeftSon = newNode; newNode.Parent = currNode; Splay(currNode); return; }
                     else { currNode = currNode.LeftSon; continue; }
                 }
             }
@@ -45,18 +48,68 @@ namespace SplayTree
                 length++;
                 int currKey = currNode.Key;
 
-                if (key == currKey) { Console.WriteLine($"S {length}"); return; }
+                if (key == currKey) { Console.WriteLine($"S {length}"); Splay(currNode); return; }
                 else if (key > currKey)
                 {
-                    if (currNode.RightSon == null) { Console.WriteLine($"F {length}"); return; }
+                    if (currNode.RightSon == null) { Console.WriteLine($"F {length}"); Splay(currNode); return; }
                     else { currNode = currNode.RightSon; continue; }
                 }
                 else if (key < currKey)
                 {
-                    if (currNode.LeftSon == null) { Console.WriteLine($"F {length}"); return; }
+                    if (currNode.LeftSon == null) { Console.WriteLine($"F {length}"); Splay(currNode); return; }
                     else { currNode = currNode.LeftSon; continue; }
                 }
 
+            }
+        }
+
+        private void Splay(SplayNode node)
+        {
+            while(node.Parent != null)
+            {
+#if SIMPLESPLAY
+                if(node.Parent == Root)
+#endif
+                {
+                    if (Root.LeftSon == node) {  L(node); }
+                    else { R(node); }
+                }
+            }
+            Root = node;
+        }
+
+        private void L(SplayNode node)
+        {
+            var originalParent = node.Parent;
+            ConnectNewParent(node, originalParent);
+
+            originalParent.LeftSon = node.RightSon;
+            if (node.RightSon != null) { node.RightSon.Parent = originalParent; }
+
+            originalParent.Parent = node;
+            node.RightSon = originalParent;
+        }
+
+        private void R(SplayNode node)
+        {
+            var originalParent = node.Parent;
+            ConnectNewParent(node, originalParent);
+
+            originalParent.RightSon = node.LeftSon;
+            if (node.LeftSon != null) { node.LeftSon.Parent = originalParent; }
+
+            originalParent.Parent = node;
+            node.LeftSon = originalParent;
+
+        }
+
+        private static void ConnectNewParent(SplayNode node, SplayNode originalParent)
+        {
+            node.Parent = originalParent.Parent;
+            if (originalParent.Parent != null)
+            {
+                if (originalParent.Parent.LeftSon == originalParent) { originalParent.Parent.LeftSon = node; }
+                else { originalParent.Parent.RightSon = node; }
             }
         }
     }
