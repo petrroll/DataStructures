ab7775962dce6fabee338489c8231ebec26c0220
Petr Houška <houskape@gmail.com>
2017-10-16 21:50:38 +0200

    Add script that produces the graphs.

---
+++ SplayTree/graph.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+
+output="graph.svg"
+title="Average path length on Find in SplayTree"
+logScale=""
+path=$(pwd)
+scale=1
+barsEvery=20
+while [ "$1" != "" ]; do
+	case "$1" in
+	-logFileNames)
+        shift
+	    logFileNames=$1
+	    ;;
+	-legendNames)
+	    shift
+	    legendNames=$1
+	    ;;
+	-output)
+	    shift
+	    output=$1
+	    ;;
+	-title)
+	    shift
+	    title=$1
+	    ;;
+	-logScale)
+	    shift
+	    logScale=$1
+	    ;;
+	-path)
+	    shift
+	    path=$1
+	    ;;
+	-limit)
+	    shift
+	    limit=$1
+	    ;;
+    esac
+    shift
+done
+
+if [ -z $logScale ]; then
+    plot="unset logscale\n"
+else
+    plot="set logscale $logScale\n"
+fi
+
+
+plot=${plot}"set term svg solid lw 1\n\
+set output \"$output\"\n\
+set grid\n\
+set title \"$title\"\n\
+set xlabel \"Number of elements in tree\"\n\
+set ylabel \"Average Find path length\"\n\
+plot [:$limit]"
+
+logFileNames=`echo $logFileNames | tr ',' ' '`
+
+color=0
+for logName in $logFileNames ; do
+    ((color++))
+    name=`echo $legendNames | cut -d',' -f $color`
+
+    plot=${plot}"\"${path}/${logName}\" using (\$1/${scale}):2 w l title \"$name\" ls 1 lc $color,"
+done
+
+plot=`echo $plot | sed 's/.$//'`
+
+plot="$plot \n\
+set output\n\
+set term pop\n"
+
+echo -e $plot | gnuplot
