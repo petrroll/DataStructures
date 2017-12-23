4cc158fbdb8077f19299275dc8fcc46609b08335
Petr Houška <houskape@gmail.com>
2017-12-23 21:38:49 +0100

    Implemented cuckoo hashing.

--- Hashing/HashTables.cs
+++ Hashing/HashTables.cs
@@ -5,24 +5,26 @@ using System.Text;
 
 namespace Hashing
 {
-    public abstract class HashTable<T>
+
+    public interface IHashTable<TValue, TKey>
     {
-        protected EqualityComparer<T> comparer;
-        protected KeyValuePair<ulong, T>[] dataStore;
+        void Add(TKey key, TValue value);
+        TValue Find(TKey key);
+
+    }
+    public abstract class HashTable<TValue, TKey>
+    {
+        protected EqualityComparer<TValue> comparer;
+        protected KeyValuePair<TKey, TValue>[] dataStore;
 
         public HashTable(int logOfCapacity)
         {
-            dataStore = new KeyValuePair<ulong, T>[1 << logOfCapacity];
-            comparer = EqualityComparer<T>.Default;
-        }
-
-        protected bool IsEmpty(int index)
-        {
-            return comparer.Equals(dataStore[index].Value, default(T));
+            dataStore = new KeyValuePair<TKey, TValue>[1 << logOfCapacity];
+            comparer = EqualityComparer<TValue>.Default;
         }
     }
 
-    public sealed class LinearHashTable<T> : HashTable<T> where T : IEquatable<T>
+    public sealed class LinearHashTable<T> : HashTable<T, ulong>, IHashTable<T, ulong> where T : IEquatable<T>
     {
         IHashFunction hashFunction;
 
@@ -60,12 +62,21 @@ namespace Hashing
 
             return dataStore[index].Value;
         }
+
+        protected bool IsEmpty(int index)
+        {
+            return comparer.Equals(dataStore[index].Value, default(T));
+        }
     }
 
-    public sealed class CuckooHashTable<T> : HashTable<T>
+    public enum HashedWith { Empty = 0, FunctionA, FunctionB }
+    public sealed class CuckooHashTable<T> : HashTable<T, (ulong key, HashedWith type)>, IHashTable<T, ulong>
     {
         IHashFunction functionA;
         IHashFunction functionB;
+
+        private int numberOfElements = 0;
+        private int rebuildTreshold = 0;
 
         public CuckooHashTable(int logOfCapacity, IHashFunction functionA, IHashFunction functionB)
             :base(logOfCapacity)
@@ -76,17 +87,127 @@ namespace Hashing
 
         public void Add(ulong key, T value)
         {
-            // Do not allow inserting default(T) values
-            Debug.Assert(!comparer.Equals(value, default(T)));
+            numberOfElements++;
+            rebuildTreshold = (int)Math.Ceiling(6 * Math.Log(numberOfElements, 2));
 
+            if (!TryToAdd(key, value))
+            {
+                Rebuild(key, value);
+            }
+        }
+
+        private bool TryToAdd(ulong key, T value)
+        {
             int hashA = functionA.Hash(key);
-            if (IsEmpty(hashA)) { dataStore[hashA] = new KeyValuePair<ulong, T>(key, value); return; }
+            if (IsEmpty(hashA)) { dataStore[hashA] = new KeyValuePair<(ulong key, HashedWith type), T>((key, HashedWith.FunctionA), value); return true; }
 
             int hashB = functionB.Hash(key);
+            return TryToAddAndPotentiallyMoveOriginal(key, value, hashB);
+        }
+
+        private bool TryToAddAndPotentiallyMoveOriginal(ulong key, T value, int index)
+        {
+            int movedElements = 0;
+
+            var elementToBeMoved = dataStore[index];
+            dataStore[index] = new KeyValuePair<(ulong key, HashedWith type), T>((key, HashedWith.FunctionB), value);
+
+            while (elementToBeMoved.Key.type != HashedWith.Empty)
+            {
+                movedElements++;
+                if (movedElements > rebuildTreshold)
+                {
+                    // The element we tried to add originally has to be on one of the following indexes
+                    // .. find out where and replace with with the element currently being moved to 
+                    // .. put the dataStore into pre-Add state for the rebuild process. 
+                    var hashA = functionA.Hash(key);
+                    if (dataStore[hashA].Key.key == key) { dataStore[hashA] = elementToBeMoved; return false; }
+
+
+                    var hashB = functionB.Hash(key);
+                    Debug.Assert(dataStore[hashB].Key.key == key);
+                    dataStore[hashB] = elementToBeMoved;
+                    return false;
+                }
+
+                var newFunction = HashedWith.Empty;
+                var newIndex = 0;
+
+                if (elementToBeMoved.Key.type == HashedWith.FunctionA)
+                {
+                    newFunction = HashedWith.FunctionB;
+                    newIndex = functionB.Hash(elementToBeMoved.Key.key);
+                }
+                else
+                {
+                    newFunction = HashedWith.FunctionA;
+                    newIndex = functionA.Hash(elementToBeMoved.Key.key);
+                }
+
+                var newElementToBeMoved = dataStore[newIndex];
+                dataStore[newIndex] = new KeyValuePair<(ulong key, HashedWith type), T>((elementToBeMoved.Key.key, newFunction), elementToBeMoved.Value);
+                elementToBeMoved = newElementToBeMoved;
+            }
+
+            return true;
+
+        }
+
+        private void Rebuild(ulong key, T value)
+        {
+            var originalDataStore = dataStore;
+            dataStore = new KeyValuePair<(ulong key, HashedWith type), T>[originalDataStore.Length];
+
+            while (true)
+            {
+                functionA.Reset();
+                functionB.Reset();
+
+                bool rebuildFailed = false;
+
+                // add the element that caused the rebuild first (it's guaranteed to succeed)
+                TryToAdd(key, value);
+
+                // add all original elements from the dataStore
+                for (int i = 0; i < originalDataStore.Length; i++)
+                {
+                    if (originalDataStore[i].Key.type == HashedWith.Empty) { continue; }
+                    if (!TryToAdd(originalDataStore[i].Key.key, originalDataStore[i].Value))
+                    {
+                        CleanDataStore();
+                        rebuildFailed = true;
+                        break;
+                    }
+                }
+
+                // when all are added, break the infinate loop and return from this function
+                if (!rebuildFailed)
+                { return; }
+            }
+        }
+
+        private void CleanDataStore()
+        {
+            for (int j = 0; j < dataStore.Length; j++)
+            {
+                dataStore[j] = new KeyValuePair<(ulong key, HashedWith type), T>();
+            }
+        }
+
+        private bool IsEmpty(int index)
+        {
+            var dataStorePackage = dataStore[index];
+            return (dataStorePackage.Key.type == HashedWith.Empty);
         }
 
         public T Find(ulong key)
         {
+            var hashA = functionA.Hash(key);
+            if(dataStore[hashA].Key.key == key && dataStore[hashA].Key.type != HashedWith.Empty) { return dataStore[hashA].Value; }
+
+            var hashB = functionB.Hash(key);
+            if (dataStore[hashB].Key.key == key && dataStore[hashB].Key.type != HashedWith.Empty) { return dataStore[hashB].Value; }
+
             return default(T);
         }
     }
--- HashingTests/HashFunctionTests.cs
+++ HashingTests/HashFunctionTests.cs
@@ -8,55 +8,43 @@ namespace HashingTests
     public class HashFunctionTests
     {
         [Fact]
-        public void NaiveModuloHashTest()
+        public void NaiveModuloHashLinTest()
         {
             LinearHashTable<ulong> myHashTable = new LinearHashTable<ulong>(10, new NaiveModuloHash(10));
-
-            for (ulong i = 0; i < 100; i++)
-            {
-                ulong otherI = (ulong)ulong.MaxValue - i;
-
-                myHashTable.Add(i + 1, i + 1);
-                myHashTable.Add(otherI, otherI);
-            }
-
-            for (ulong i = 0; i < 100; i++)
-            {
-                ulong otherI = (ulong)ulong.MaxValue - i;
-
-                Assert.Equal(i + 1, myHashTable.Find(i + 1));
-                Assert.Equal(otherI, myHashTable.Find(otherI));
-            }
+            TestFirstAndLastNElements(myHashTable, 300);
         }
 
         [Fact]
-        public void TableHashTest()
+        public void TableHashLinTest()
         {
             LinearHashTable<ulong> myHashTable = new LinearHashTable<ulong>(10, new TableHash(10, 16));
-
-            for (ulong i = 0; i < 100; i++)
-            {
-                ulong otherI = (ulong)ulong.MaxValue - i;
-
-                myHashTable.Add(i + 1, i + 1);
-                myHashTable.Add(otherI, otherI);
-            }
-
-            for (ulong i = 0; i < 100; i++)
-            {
-                ulong otherI = (ulong)ulong.MaxValue - i;
-
-                Assert.Equal(i + 1, myHashTable.Find(i + 1));
-                Assert.Equal(otherI, myHashTable.Find(otherI));
-            }
+            TestFirstAndLastNElements(myHashTable, 300);
         }
 
         [Fact]
-        public void MultiShiftHashTest()
+        public void MultiShiftHashLinTest()
         {
             LinearHashTable<ulong> myHashTable = new LinearHashTable<ulong>(10, new MultiShiftHash(10));
+            TestFirstAndLastNElements(myHashTable, 300);
+        }
 
-            for (ulong i = 0; i < 100; i++)
+        [Fact]
+        public void MultiShiftHashCuckooTest()
+        {
+            var myHashTable = new CuckooHashTable<ulong>(10, new MultiShiftHash(10), new MultiShiftHash(10));
+            TestFirstAndLastNElements(myHashTable, 300);
+        }
+
+        [Fact]
+        public void TableHashCuckooTest()
+        {
+            var myHashTable = new CuckooHashTable<ulong>(10, new TableHash(10, 16), new TableHash(10, 16));
+            TestFirstAndLastNElements(myHashTable, 300);
+        }
+
+        private static void TestFirstAndLastNElements(IHashTable<ulong, ulong> myHashTable, ulong numberOfElements)
+        {
+            for (ulong i = 0; i < numberOfElements; i++)
             {
                 ulong otherI = (ulong)ulong.MaxValue - i;
 
@@ -64,7 +52,7 @@ namespace HashingTests
                 myHashTable.Add(otherI, otherI);
             }
 
-            for (ulong i = 0; i < 100; i++)
+            for (ulong i = 0; i < numberOfElements; i++)
             {
                 ulong otherI = (ulong)ulong.MaxValue - i;
 
